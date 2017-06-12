57b50655e46b74f07219f9bf93890faa2891c745
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-06-12 14:57:08 -0500

    User Input Exercise Finished W/ PrepareStatement (Secured)!

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,11 +1,11 @@
 package com.codeup.adlister.dao;
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 import com.mysql.jdbc.Driver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by daniel on 6/9/17.
@@ -30,18 +30,48 @@ public class MySQLUsersDao implements Users {
     }
 
     @Override
-    public Long insertUser(User user) throws SQLException {
-        String userName = user.getUsername();
-        String userEmail = user.getEmail();
-        String userPassword = user.getPassword();
+    public Long insertUser(User user) {
+//        String userName = user.getUsername();
+//        String userEmail = user.getEmail();
+//        String userPassword = user.getPassword();
+//
+//        Statement stmt = connection.createStatement();
+//
+//        String query = "INSERT INTO users(username , email, password) VALUES";
+//        query += "('" + userName + "', '" + userEmail + "', '" + userPassword + "')";
+//        System.out.println(query);
+//        stmt.execute(query);
 
-        Statement stmt = connection.createStatement();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(
+                "INSERT INTO users(username , email, password) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
+        }
+    }
 
-        String query = "INSERT INTO users(username , email, password) VALUES";
-        query += "('" + userName + "', '" + userEmail + "', '" + userPassword + "')";
-        System.out.println(query);
-        stmt.execute(query);
+    private User extractUser(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
+        );
+    }
 
-        return null;
+    private List<User> createUsersFromResults(ResultSet rs) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (rs.next()) {
+            users.add(extractUser(rs));
+        }
+        return users;
     }
 }
